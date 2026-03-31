e9a5dc011f64753cd0c15c25ec3a9208f65ca294
Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
2026-03-31 07:55:39 +0200

    Restore menu modifier exports in core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,6 @@
 export * from './enums';
 export * from './i18n';
+export * from './menu-modifier-groups';
 export * from './order-closure';
 export * from './p24-metadata';
 export * from './pickup-time-adjustments';
